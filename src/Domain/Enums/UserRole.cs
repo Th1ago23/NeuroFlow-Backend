4a23d8d7ff707600cc62879b075cdffefe7bde4d
Thiago Peixoto <thiago.peixots@gmail.com>
2025-11-19 17:20:54 -0300

    Adicionar arquivos de projeto.

---
+++ src/Domain/Enums/UserRole.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Enums;
+
+public enum UserRole
+{
+    Admin = 1,
+    Psychologist = 2,
+    Psychiatrist = 3,
+    Patient = 4,
+    Assistent = 5,
+}
---
+++ src/Infrastructure/Config/DbConfig.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Config;
+
+public class DbConfig:DbContext
+{
+    public DbSet<>
+}
